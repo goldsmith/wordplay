f5ff881da026a9bb55c3dc4f84d59bc4dc2730f1
Jonathan Goldsmith <jhghank@gmail.com>
2014-05-18 19:57:37 -0700

    testing with travis?

--- test/wordplay/test/web.clj
+++ test/wordplay/test/web.clj
@@ -7,7 +7,7 @@
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "<h2>Hello World</h2>"))))
+      (is (re-find #"Hello World" (:body response)))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
