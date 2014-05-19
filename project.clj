911ab5cc9b820de54788e70b0fd64ab139367342
Jonathan Goldsmith <jhghank@gmail.com>
2014-05-18 19:23:26 -0700

    postgres dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "API for cheating at board games"
   :url "http://github.com/goldsmith/wordplay"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler wordplay.handler/app}
   :profiles
